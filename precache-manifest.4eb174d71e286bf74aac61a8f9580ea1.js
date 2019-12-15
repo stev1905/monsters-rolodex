cdc8ffd70d2ba530a2578fdedb9df6cb632e2f68
Christian Stevens <stev1905@gmail.com>
2019-12-14 23:34:43 -0500

    Updates

---
+++ precache-manifest.4eb174d71e286bf74aac61a8f9580ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8387c31a287d9c324ac7defcaeb8da6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f5b0f80983d050d0d39e",
+    "url": "/monsters-rolodex/static/css/main.fe5fe188.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5b0f80983d050d0d39e",
+    "url": "/monsters-rolodex/static/js/main.f0122f02.chunk.js"
+  },
+  {
+    "revision": "dc25ba12107eb546721c",
+    "url": "/monsters-rolodex/static/js/runtime-main.75c8e44f.js"
+  }
+]);
